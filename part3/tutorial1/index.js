f01e4bb77d76961bd920f5ec884cbec1c8895255
samniem <sami.nieminen.hw@gmail.com>
2021-08-22 09:39:58 +0300

    Update Tutorial part 3.1

--- part3/tutorial1/index.js
+++ part3/tutorial1/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
+
 const app = express()
+app.use(express.json())
 
 let notes = [
     { 
@@ -35,6 +37,36 @@ app.get('/api/notes/:id', (request, response) => {
     const note = notes.find(note => note.id === id)
     console.log(note)
     note ? response.json(note) : response.status(404).send("Entry not found")
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+    response.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
+    return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if(!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId(),
+    }
+    notes = notes.concat(note)
+    response.json(note)
 })
 
 const PORT = 3001
